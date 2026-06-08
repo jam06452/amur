71a3620be0db138cda7097a760586130f5882981
q <jam@jam06452.uk>
2026-06-08 13:54:29 +0100

    Increased version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Amur.MixProject do
   def project do
     [
       app: :amur,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.15",
       description: "Simple OAuth for Plug apps",
       deps: deps(),
