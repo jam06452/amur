75c5b397e8d19ace090e112d411b60843bbc7412
q <jam@jam06452.uk>
2026-07-24 21:17:51 +0100

    Feat: Provider Auth Token is now accessible

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Amur.MixProject do
   def project do
     [
       app: :amur,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.15",
       description: "Simple OAuth for Plug apps",
       deps: deps(),
