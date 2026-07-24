edf8845c2d04ec7fe8492365b32fe288bc572596
q <jam@jam06452.uk>
2026-07-24 13:23:51 +0100

    Fix: Bump version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Amur.MixProject do
   def project do
     [
       app: :amur,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.15",
       description: "Simple OAuth for Plug apps",
       deps: deps(),
