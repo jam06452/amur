8d118abe20c41edd03a4757c2f2f35abb506f521
jam06452 <jam@jam06452.uk>
2026-09-03 15:21:02 +0100

    chore: updated readme to reflect new additions, bumped version from 0.2.4 to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Amur.MixProject do
   def project do
     [
       app: :amur,
-      version: "0.2.4",
+      version: "0.3.0",
       elixir: "~> 1.15",
       description: "Simple OAuth for Plug apps",
       deps: deps(),
